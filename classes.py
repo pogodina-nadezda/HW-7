f9cba3fced331f0054cb9d972952c158179dbd09
pogodina-nadezda <43251140+pogodina-nadezda@users.noreply.github.com>
2018-11-05 11:35:14 +0300

    changes made after the first review
    Added blank lines, changed classes names, two lines are combined into one

--- classes.py
+++ classes.py
@@ -1,24 +1,27 @@
-class DNA(str):
+class Dna(str):
     def __init__(self, nucleotides):
         self.nucleotides = nucleotides
+        
     def gc(self):
-        self.GC_count = self.nucleotides.count('G') + self.nucleotides.count('C')
-        self.GC_content = self.GC_count / len(self.nucleotides) * 100
+        self.GC_content = (self.nucleotides.count('G') + self.nucleotides.count('C')) / len(self.nucleotides) * 100
         print(round((self.GC_content), 2), "%")
+        
     def reverse_complement(self):
         self.complement = self.maketrans('ATCG', 'TAGC')
         print(self.nucleotides.translate(self.complement))
+        
     def transcribe(self):
         self.complement_RNA = self.maketrans('ATCG', 'UAGC')
         print(self.nucleotides.translate(self.complement_RNA))
 
-class RNA(str):
+class Rna(str):
     def __init__(self, nucleotides):
         self.nucleotides = nucleotides
+        
     def gc(self):
-        self.GC_count = self.nucleotides.count('G') + self.nucleotides.count('C')
-        self.GC_content = self.GC_count / len(self.nucleotides) * 100
+        self.GC_content = (self.nucleotides.count('G') + self.nucleotides.count('C')) / len(self.nucleotides) * 100
         print(round((self.GC_content), 2), "%")
+        
     def reverse_complement(self):
         self.complement = self.maketrans('AUCG', 'UAGC')
         print(self.nucleotides.translate(self.complement))
