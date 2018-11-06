f8ff6e2034b8228dbe7142e63f2a7df20ba9de91
pogodina-nadezda <nadezda.pogodina.bio@gmail.com>
2018-11-06 22:29:51 +0300

    made class inheritance

--- classes.py
+++ classes.py
@@ -1,27 +1,20 @@
-class Dna(str):
-    def __init__(self, nucleotides):
-        self.nucleotides = nucleotides
-        
-    def gc(self):
-        self.GC_content = (self.nucleotides.count('G') + self.nucleotides.count('C')) / len(self.nucleotides) * 100
-        return(round((self.GC_content), 2), "%")
-        
-    def reverse_complement(self):
-        self.complement = self.maketrans('ATCG', 'TAGC')
-        return(self.nucleotides.translate(self.complement))
-        
-    def transcribe(self):
-        self.complement_RNA = self.maketrans('ATCG', 'UAGC')
-        return(self.nucleotides.translate(self.complement_RNA))
-
 class Rna(str):
     def __init__(self, nucleotides):
         self.nucleotides = nucleotides
-        
+
     def gc(self):
         self.GC_content = (self.nucleotides.count('G') + self.nucleotides.count('C')) / len(self.nucleotides) * 100
-        return(round((self.GC_content), 2), "%")
-        
+        return round((self.GC_content), 2)
+
     def reverse_complement(self):
-        self.complement = self.maketrans('AUCG', 'UAGC')
-        return(self.nucleotides.translate(self.complement))
+        self.complement = self.maketrans('AUTCG', 'UAAGC')
+        return self.nucleotides.translate(self.complement)
+
+
+class Dna(Rna):
+    def reverse_complement(self):
+        self.complement_Dna = self.maketrans('ATCG', 'TAGC')
+        return self.nucleotides.translate(self.complement_Dna)
+
+    def transcribe(self):
+        return Rna.reverse_complement(self)
